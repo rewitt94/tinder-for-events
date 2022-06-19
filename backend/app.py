0243c42d74603f210d45274f1aedebdf4ee2c4c6
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-06-19 15:16:25 +0100

    hardcoded event data

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,32 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 def handler(event: APIGWLambdaEvent, context: LambdaContext):
     print("Hello world")
     bucket_goes_here()
+    return [
+        { 
+            'event_name' : "Makers hackathon",
+            'event_location': "Makers Academy",
+            'event_date': "July 20th"
+        },
+        { 
+            'event_name' : "Makers anniversary",
+            'event_location': "Makers Academy",
+            'event_date': "August 28th"
+        },
+          { 
+            'event_name' : "Makers yoga",
+            'event_location': "Makers Academy",
+            'event_date': "October 15th"
+        },
+            { 
+            'event_name' : "Makers interview practice",
+            'event_location': "Makers Academy",
+            'event_date': "December 31st"
+        },
+              { 
+            'event_name' : "Makers charity ball",
+            'event_location': "Makers Academy",
+            'event_date': "February 11th"
+        }
+    ]
+    
+
